cb8c837d3de8d5cb30ae63bd62155c77a31997c6
Oleksii Kovsh <kovsh1999@gmail.com>
2023-05-27 19:18:37 +0300

    task 3,4 - done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+ingredients.forEach((ingredient) => {
+	const liIngredient = document.createElement('li');
+	liIngredient.classList.add("item");
+	liIngredient.textContent = ingredient;
+	const ulIngredients = document.querySelector('ul');
+	ulIngredients.append(liIngredient);
+}, 0);
